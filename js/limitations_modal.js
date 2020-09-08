b1c0ff323e7a63559f8dc8caa732f0f6988cfd13
Axel Gandy <a.gandy@imperial.ac.uk>
2020-09-08 22:53:32 +0000

    New run at 2020-09-08 22:53:31

--- js/limitations_modal.js
+++ js/limitations_modal.js
@@ -5,17 +5,17 @@ $(function() {
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="limitationsModalLabel">COVID-19 local predictions for Great Britain</h4>
+        <h4 class="modal-title" id="limitationsModalLabel">COVID-19 Great Britain</h4>
       </div>
       <div class="modal-body">
         <ul>
-         <li> This site give estimates of the reproduction number of COVID-19 and predictions of cases by Local Authority in Great Britain based on testing data and mortality data.
-         <li> All estimates and predictions have uncertainty/probability measures associated with them. Central estimates should be treated cautiously: look at the range.
-          <li> Predictions of cases assume that <strong>interventions (lockdowns, school closures, and others) do not change</strong> from about a week before the end of observations to the end of the prediction period. 
+         <li> This site give estimates of the reproduction number of COVID-19 and projections of cases by Local Authority in Great Britain based on testing data and mortality data.
+         <li> All estimates and projections have uncertainty/probability measures associated with them. Central estimates should be treated cautiously: look at the range.
+          <li> Projections of cases assume that <strong>interventions (e.g. lockdowns, school closures) and behaviour patterns do not change</strong> from about a week before the end of observations onwards. 
           <li>
             An increase in cases in an area can be due to an increase in testing. The model currently does not account for this.
           </li>
-          <li> A statistical model is used to estimate the reproduction number R, as it cannot be directly measured. This drives our forecasts. No model is perfect; see <strong>limitations</strong> in "Details".
+          <li> A statistical model is used to estimate qunatities that cannot be directly measures, such as the reproduction number R. No model is perfect; see <strong>limitations</strong> in "Details".
           </li>
         </ul>
       </div>
