a78a81bf97de49c077871ee71e621e35f07bd0b5
Axel Gandy <a.gandy@imperial.ac.uk>
2020-09-10 20:14:32 +0000

    New run at 2020-09-10 20:14:32

--- js/limitations_modal.js
+++ js/limitations_modal.js
@@ -20,7 +20,7 @@ $(function() {
         </ul>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal" onclick="topFunction()">Close</button>
       </div>
     </div>
   </div>
--- js/overview.js
+++ js/overview.js
@@ -20,3 +20,9 @@ function showCases(target_id) {
   
   window.dispatchEvent(new Event('resize'));
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
